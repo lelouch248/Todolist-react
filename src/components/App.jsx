087dbb4ccd947b92b6279efd9fdbd2713936d8a9
Karthik G <68554363+lelouch248@users.noreply.github.com>
2023-02-11 20:25:25 +0530

    implemented on click delete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 import Heading from "./Heading";
 import Todolist from "./Todolist";
 import Form from "./Form";
+
 function App() {
   const [inputText, setInputText] = useState("");
   const [items, setItems] = useState([]);
+
   function handleChange(event) {
     const newValue = event.target.value;
     setInputText(newValue);
@@ -16,6 +18,13 @@ function App() {
     });
   }
 
+  function deleteItem(id) {
+    setItems((prevItems) => {
+      return prevItems.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
   return (
     <div className="container">
       <Heading />
@@ -24,7 +33,18 @@ function App() {
         inputText={inputText}
         addItem={addItem}
       />
-      <Todolist items={items} />
+      <div>
+        <ul>
+          {items.map((todoitem, index) => (
+            <Todolist
+              key={index}
+              id={index}
+              text={todoitem}
+              onChecked={deleteItem}
+            />
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
--- src/components/Todolist.jsx
+++ src/components/Todolist.jsx
@@ -1,22 +1,13 @@
 import React, { useState } from "react";
 
 function Todolist(props) {
-  const [itemclick, setItemClick] = useState(false);
-  function handleClick() {
-    setItemClick(!itemclick);
-  }
   return (
-    <div>
-      <ul>
-        {props.items.map((todoitem) => (
-          <li
-            onClick={handleClick}
-            style={{ textDecoration: itemclick ? "line-through" : "none" }}
-          >
-            {todoitem}
-          </li>
-        ))}
-      </ul>
+    <div
+      onClick={() => {
+        props.onChecked(props.id);
+      }}
+    >
+      <li>{props.text}</li>
     </div>
   );
 }
